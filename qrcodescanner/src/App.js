cceafb74056e0f0ccd3601536dda7cd8b0c637ea
Wassim <laabidi.wassim250@gmail.com>
2022-08-14 06:24:20 -0700

    initialie qr code scanner

--- qrcodescanner/src/App.js
+++ qrcodescanner/src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
+import React, {useState} from 'react';
+import {Container, Card, CardContent, makeStyles, Grid} from '@material-ui/core';
+import {QrScanner} from "react-qrcode-scanner";
 
 function App() {
+
+    const classes = useStyles();
+    const [scanResultWebCam, setScanResultWebCam] =  useState('');
+
+    const handleScan = (result) => {
+        if (result){
+            setScanResultWebCam(result);
+        }
+    }
+
+    const handleError = (error) => {
+        console.log({error})
+    }
+
     return (
         <div className="App">
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <Container className={classes.container}>
+                <Card>
+                    <CardContent className='qr-scan'>
+                        <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                            <h3 className='title'>Qr Code Scanner</h3>
+                                    <QrScanner
+                                        onScan={handleScan}
+                                        onError={handleError}
+                                    />
+                            <p>Scanned Code: {scanResultWebCam}</p>
+                        </Grid>
+                    </CardContent>
+                </Card>
+            </Container>
         </div>
     );
 }
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+      marginTop: 10
+    }
+}));
 
 export default App;
